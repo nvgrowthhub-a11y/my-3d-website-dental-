2c6a9534e6722fc84e2145598358185ce0152922
nvgrowthhub-a11y <nvgrowthhub@gmail.com>
2026-07-22 15:10:58 +0530

    Update Lights.tsx

--- src/components/canvas/Lights.tsx
+++ src/components/canvas/Lights.tsx
@@ -1,1 +1,12 @@
+'use client';
 
+export default function Lights() {
+  return (
+    <>
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[10, 10, 5]} intensity={1.8} color="#ffffff" />
+      <pointLight position={[-10, -10, -5]} intensity={1.2} color="#6366f1" />
+      <pointLight position={[5, -5, 5]} intensity={0.8} color="#ec4899" />
+    </>
+  );
+}
