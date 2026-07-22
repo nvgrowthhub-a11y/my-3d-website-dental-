d001818821bca462fd94a4d194342a01e9150fee
nvgrowthhub-a11y <nvgrowthhub@gmail.com>
2026-07-22 15:11:28 +0530

    Update Scene.tsx

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,1 +1,20 @@
+'use client';
 
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Float } from '@react-three/drei';
+import Lights from './Lights';
+import AnimatedMesh from './AnimatedMesh';
+
+export default function Scene() {
+  return (
+    <div className="absolute inset-0 z-0 h-full w-full">
+      <Canvas camera={{ position: [0, 0, 5], fov: 60 }}>
+        <Lights />
+        <Float speed={2} rotationIntensity={1} floatIntensity={1.5}>
+          <AnimatedMesh />
+        </Float>
+        <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={0.8} />
+      </Canvas>
+    </div>
+  );
+}
